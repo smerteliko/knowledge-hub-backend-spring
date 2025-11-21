e725f5fb36bf6c220be8b4b05cff657bf3a18133
smerteliko <smerteliko@gmail.com>
2025-11-21 17:59:23 +0300

    feat(jwt): Implement JwtService for token generation and define Auth DTOs

---
+++ src/main/java/com/smerteliko/knowledgehub/dto/auth/JwtResponse.java
@@ -0,0 +1,14 @@
+package com.smerteliko.knowledgehub.dto.auth;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class JwtResponse {
+    private String token;
+    private String username;
+    private Long userId;
+}
